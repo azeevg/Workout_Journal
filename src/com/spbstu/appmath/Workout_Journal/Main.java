1fcdab91f088c6ba080bb2d2176ef34df56b5a32
Gleb Azeev <azeevg@gmail.com>
2016-04-10 21:34:11 +0300

    Make layouts more like

--- src/com/spbstu/appmath/Workout_Journal/Main.java
+++ src/com/spbstu/appmath/Workout_Journal/Main.java
@@ -2,49 +2,57 @@ package com.spbstu.appmath.Workout_Journal;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.*;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+
 
 public class Main extends Activity {
 
     public static final String NAME = "name";
     public static final String DATE = "date";
-    private ArrayList<HashMap<String, Object>> historyItems;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.history);
-//      String[] trainings = getResources().getStringArray(R.array.trainings);
-//        String[] trainings = getResources().getStringArray(R.array.exercises);
+        setContentView(R.layout.history_removing);
         ListView listView = (ListView) findViewById(R.id.historyListView);
-
-        historyItems = new ArrayList<HashMap<String, Object>>();
-        HashMap<String, Object> hm;
+        final ArrayList<HashMap<String, String>> historyItems = new ArrayList<>();
+        HashMap<String, String> hm;
 
         for (int i = 1; i < 10; i++) {
             hm = new HashMap<>();
             hm.put(NAME, "Название тренировки " + i);
             hm.put(DATE, "1" + i + ".04.2016");
             historyItems.add(hm);
+            System.out.println(hm);
         }
+
         SimpleAdapter adapter = new SimpleAdapter(this, historyItems,
-                R.layout.history_list_item, new String[]{"NAME", "DATE"},
+                R.layout.history_list_item, new String[]{NAME, DATE},
                 new int[]{R.id.historyItemName, R.id.historyItemDate});
 
         listView.setAdapter(adapter);
 
+//        String[] trainings = getResources().getStringArray(R.array.exercises);
 
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.history_list_item, trainings);
-//        listView.setAdapter(adapter);
-       /* listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                CheckedTextView textView = (CheckedTextView) view.findViewById(view.getId());
-                textView.setChecked(!textView.isChecked());
-            }
-        });*/
-//        setContentView(R.layout.not_main);
+        /*String[] trainings = getResources().getStringArray(R.array.trainings);
+        HashMap<String, String> hm = null;
+        ArrayList<Map<String, String>> workouts = new ArrayList<>();
+        for (String workout : trainings) {
+            hm = new HashMap<>();
+            hm.put(NAME, workout);
+            workouts.add(hm);
+        }
+        setContentView(R.layout.main_removing);
+        ListView listView = (ListView) findViewById(R.id.mainTrainList);
+        SimpleAdapter adapter = new SimpleAdapter(this, workouts, R.layout.list_item,
+                new String[]{NAME}, new int[]{R.id.listItem});
+        listView.setAdapter(adapter);*/
+
     }
 }
